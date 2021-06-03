01aaa02e02486587be5386f18530966ed3d1a18d
Edoardo Scibilia <edoardoscibilia@gmail.com>
2021-06-03 18:35:58 +0200

    fucking around

--- OrbiterGame/Assets/Scripts/SpawnerController.cs
+++ OrbiterGame/Assets/Scripts/SpawnerController.cs
@@ -6,11 +6,32 @@ public class SpawnerController : MonoBehaviour
 {
     [SerializeField] public GameObject asteroidPrefab = null;
 
+    [SerializeField] private float _timetoSpawn = 0f;
+    [SerializeField] private float _timetoNextSpawn = 0f;
+
     private void Start()
     {
-        Instantiate(
-            asteroidPrefab,
-            transform.position,
-            transform.rotation);
+        SpawnAsteroid();
+    }
+
+    private void Update()
+    {
+        TimedSpawnAsteroid();
+    }
+
+    private void TimedSpawnAsteroid()
+    {
+        if (Time.time >= _timetoSpawn)
+        {
+            SpawnAsteroid();
+            _timetoSpawn += _timetoNextSpawn;
+        }
+    }
+
+    private void SpawnAsteroid()
+    {
+        Instantiate(asteroidPrefab,
+                    transform.position,
+                    transform.rotation);
     }
 }
